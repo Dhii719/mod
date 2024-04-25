4986cc94d23777f3993aeb6ae1261ee3b5847da0
Dhii719 <151818487+Dhii719@users.noreply.github.com>
2024-04-25 22:26:14 +0700

    Update remote-ssh.sh

--- remote-ssh.sh
+++ remote-ssh.sh
@@ -1,1 +1,13 @@
+bash
+#!/bin/bash
 
+TELEGRAM_TOKEN="TOKEN_BOT_KAMU"
+CHAT_ID="ID_CHAT_TELEGRAM_KAMU"
+
+if [ "$1" == "run" ]; then
+  shift
+  ssh user@host "$@"
+else
+  MESSAGE="Perintah SSH diterima: $*"
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d text="$MESSAGE"
+fi
